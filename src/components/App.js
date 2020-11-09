c8e0c9f095c7778bce74b9388b8a3d87eef33b49
Mratyunjay Tripathi <tripathi.mratyunjay.1@gmail.com>
2020-11-09 18:22:58 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <></>
-  )
+    <div>
+      <p data-ns-test="project-name">First React project </p>
+      <p data-ns-test="project-description">This project is a demo project just dipicting functionality of each individual elements</p>
+    </div>
+  );
 }
 
 
